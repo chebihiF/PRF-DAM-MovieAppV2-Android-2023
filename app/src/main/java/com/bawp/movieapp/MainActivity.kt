e79049670982baa28e3c829daf10b7516747df46
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-05-17 14:28:17 +0100

    add movie event

--- app/src/main/java/com/bawp/movieapp/MainActivity.kt
+++ app/src/main/java/com/bawp/movieapp/MainActivity.kt
@@ -2,8 +2,10 @@ package com.bawp.movieapp
 
 import android.graphics.drawable.Icon
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -65,19 +67,24 @@ fun MainContent(moviesList: List<String> = listOf(
     Column(modifier = Modifier.padding(12.dp)) {
         LazyColumn{
             items(items = moviesList){
-                MovieRow(movie = it)
+                MovieRow(movie = it){ movie ->
+                    Log.d("TAG","MainContent : $movie")
+                }
             }
         }
     }
 }
 
 @Composable
-fun MovieRow(movie: String) {
+fun MovieRow(movie: String, onItemClick: (String) -> Unit = {}) {
     Card(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(130.dp),
+            .height(130.dp)
+            .clickable {
+                onItemClick(movie)
+            },
         elevation = 16.dp,
         shape = RoundedCornerShape(corner = CornerSize(16.dp)))
     {
