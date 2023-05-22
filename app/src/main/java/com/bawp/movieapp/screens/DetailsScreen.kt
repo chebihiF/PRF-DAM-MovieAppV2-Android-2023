f382ba1bd89746ca35a2b65f396e0a75fd31b4f7
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-05-22 15:49:53 +0100

    final version

--- app/src/main/java/com/bawp/movieapp/screens/DetailsScreen.kt
+++ app/src/main/java/com/bawp/movieapp/screens/DetailsScreen.kt
@@ -1,15 +1,27 @@
 package com.bawp.movieapp.screens
 
+import android.app.Activity
+import android.app.Application
+import android.content.Context
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -22,11 +34,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
+import com.bawp.movieapp.model.Movie
+import com.bawp.movieapp.model.getMovies
+import com.bawp.movieapp.widgets.MovieRow
+
 
 @Composable
-fun DetailsScreen(navController: NavController, movieData: String?){
+fun DetailsScreen(navController: NavController, movieId: String?){
+
+    val movieDetails = getMovies().filter { movie -> movie.id == movieId }[0]
+
     Scaffold(topBar = {
         TopAppBar(backgroundColor = Color.Transparent, elevation = 0.dp) {
             Row(horizontalArrangement = Arrangement.Start) {
@@ -41,20 +63,37 @@ fun DetailsScreen(navController: NavController, movieData: String?){
 
         }
     }) {
-        Text(text = movieData.toString(), style = MaterialTheme.typography.h5)
-    }
-
-/*Surface(modifier = Modifier
-        .fillMaxHeight()
-        .fillMaxWidth()) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-            Text(text = movieData.toString(), style = MaterialTheme.typography.h5)
-            Spacer(modifier = Modifier.height(23.dp))
-            Button(onClick = {
-                navController.popBackStack()
-            }) {
-                Text(text = "Go Back")
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top) {
+                MovieRow(movie = movieDetails)
+                Spacer(modifier = Modifier.height(8.dp))
+                Divider()
+                Text(text = "Movie Images")
+                HorizontalScrollableImageView(movieDetails)
             }
         }
-    }*/
+    }
+
+
+}
+
+@Composable
+private fun HorizontalScrollableImageView(movieDetails: Movie) {
+    LazyColumn {
+        items(movieDetails.images) { image ->
+            Card(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(240.dp),
+                elevation = 5.dp
+            ) {
+                Image(
+                    contentScale = ContentScale.Crop,
+                    painter = rememberImagePainter(data = image),
+                    contentDescription = "Movie Poster"
+                )
+            }
+        }
+    }
 }
--- app/src/main/java/com/bawp/movieapp/widgets/widgets.kt
+++ app/src/main/java/com/bawp/movieapp/widgets/widgets.kt
@@ -1,5 +1,7 @@
 package com.bawp.movieapp.widgets
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -12,33 +14,49 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Divider
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
 import coil.size.Scale
 import coil.transform.CircleCropTransformation
 import com.bawp.movieapp.model.Movie
 import com.bawp.movieapp.model.getMovies
 
+@OptIn(ExperimentalAnimationApi::class)
 @Preview
 @Composable
 fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) {
+    var expended by remember {
+        mutableStateOf(false)
+    }
     Card(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(130.dp)
             .clickable {
                 onItemClick(movie.id)
             },
@@ -67,6 +85,38 @@ fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) 
                     style = MaterialTheme.typography.caption)
                 Text(text = "Released: ${movie.year}",
                     style = MaterialTheme.typography.caption)
+
+                AnimatedVisibility(visible = expended) {
+                    Column {
+                        Text(buildAnnotatedString {
+                            withStyle(style = SpanStyle(color = Color.DarkGray,
+                            fontSize = 13.sp)){
+                                append("Plot: ")
+                            }
+                            withStyle(style = SpanStyle(color = Color.DarkGray,
+                            fontSize = 13.sp, fontWeight = FontWeight.Bold
+                            )){
+                                append(movie.plot)
+                            }
+                        })
+                        Divider(modifier = Modifier.padding(6.dp))
+                        Text(text = "Actors: ${movie.actors}", style = MaterialTheme.typography.caption)
+                        Text(text = "Rating: ${movie.rating}", style = MaterialTheme.typography.caption)
+                    }
+                }
+
+
+                Icon(
+                    imageVector = if(expended) Icons.Filled.KeyboardArrowUp
+                    else Icons.Filled.KeyboardArrowDown,
+                    contentDescription = "Down Arrow",
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable {
+                            expended = !expended
+                        },
+                    tint = Color.DarkGray
+                )
             }
         }
 
